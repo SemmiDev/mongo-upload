5e16a71f99509aab9ee2a20fe5fe09741004ddf4
SemmiDev <sammidev4@gmail.com>
2021-06-10 18:40:17 +0700

    init

---
+++ src/main/java/com/sammidev/SpringbootMongoUploadApplication.java
@@ -0,0 +1,12 @@
+package com.sammidev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongoUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMongoUploadApplication.class, args);
+	}
+}
---
+++ src/main/java/com/sammidev/model/LoadFile.java
@@ -0,0 +1,43 @@
+package com.sammidev.model;
+
+public class LoadFile {
+    private String fileName;
+    private String fileType;
+    private String fileSize;
+    private byte[] file;
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public String getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(String fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public byte[] getFile() {
+        return file;
+    }
+
+    public void setFile(byte[] file) {
+        this.file = file;
+    }
+
+    public LoadFile() {
+    }
+}
